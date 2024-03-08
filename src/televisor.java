3c2c87dc6e385d6937eb026575d55f3f3fd0a117
Luque-K <kjluquea@udsitrital.edu.co>
2024-03-08 07:48:27 -0500

    creandoTV

--- src/televisor.java
+++ src/televisor.java
@@ -1,4 +1,56 @@
 
 public class televisor {
 
+	 private String marca , tamaño, volumen,canal ,entradas , encendido;
+//c
+	 public void encender () {};
+	 public void volmas () {};
+	 public void volmen () {} ;
+	 public void canalmas () {};
+	 public void canalmen () {}
+	
+	 //g y s
+	 
+	 public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getTamaño() {
+		return tamaño;
+	}
+	public void setTamaño(String tamaño) {
+		this.tamaño = tamaño;
+	}
+	public String getVolumen() {
+		return volumen;
+	}
+	public void setVolumen(String volumen) {
+		this.volumen = volumen;
+	}
+	public String getCanal() {
+		return canal;
+	}
+	public void setCanal(String canal) {
+		this.canal = canal;
+	}
+	public String getEntradas() {
+		return entradas;
+	}
+	public void setEntradas(String entradas) {
+		this.entradas = entradas;
+	}
+	public String getEncendido() {
+		return encendido;
+	}
+	public void setEncendido(String encendido) {
+		this.encendido = encendido;
+	};
+	 
+
+	
+	
+	
 }
+
